a1b47cb4db57d24c86adf4e872622627a925d684
Caioxlw <139200279+Caioxlw@users.noreply.github.com>
2025-01-27 22:04:20 -0300

    Add files via upload

---
+++ dados.py
@@ -0,0 +1,29 @@
+
+
+
+global dadosProduto
+dadosProduto = {
+    'ID_PRODUTO':[],
+    'NOME_PRODUTO':[],
+    'QUANTIDADE_PRODUTO':[]    
+} 
+   
+global dadosCliente
+dadosCliente = {
+    'ID_CLIENTE':[],
+    'NOME_CLIENTE':[]
+} 
+
+global dadosFornecedor
+dadosFornecedor = {
+    'ID_FORNECEDOR':[],
+    'NOME_FORNECEDOR':[]
+}
+
+global dadosVendas
+dadosVendas = {
+    'ID_VENDAS':[],
+    'NOME_VENDA':[],
+    'VALOR_VENDA':[]
+}
+
