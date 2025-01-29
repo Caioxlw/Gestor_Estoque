b691d4e66bbc040cde11a878b372f2587489e800
Pedro Xavier <163337475+XavData@users.noreply.github.com>
2025-01-29 14:36:16 -0300

    Delete GestorEstoque directory

--- GestorEstoque/main.py
+++
@@ -1,7 +0,0 @@
-from menu_principal import menuPrincipal
-
-try:
-    menuPrincipal()
-except KeyboardInterrupt:
-    print('\n\nFechando codigo...\n\n')
-    
--- GestorEstoque/menu_vendas.py
+++
@@ -1,5 +0,0 @@
-import os
-from time import sleep
-
-def menuVendas():
-    pass
